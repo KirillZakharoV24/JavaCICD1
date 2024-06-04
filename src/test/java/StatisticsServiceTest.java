3b435acd9cb05aac16a5c333baebb0be6110304c
KirillZakharoV24 <156929709+KirillZakharoV24@users.noreply.github.com>
2024-06-04 20:04:03 +0500

    Update StatisticsServiceTest.java

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -14,6 +14,17 @@ public class StatisticsServiceTest {
 
         long actual = service.findMax(incomesInBillions);
 
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
+    
+     @Test
+    void findnotMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
 }
